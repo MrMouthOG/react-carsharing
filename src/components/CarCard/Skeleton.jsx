fa4b766b9b56c201066e7f56e556a31c29674818
Dmitrii <dmitriialekseenkoog@gmail.com>
2022-10-19 12:27:58 +0400

    add skeleton for cars

---
+++ src/components/CarCard/Skeleton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+import styles from './CarCard.module.scss';
+
+function Skeleton() {
+  return (
+    <ContentLoader
+      // className={styles.rentItem}
+      speed={2}
+      width={1112}
+      height={135}
+      viewBox="0 0 1112 135"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb">
+      <rect x="15" y="15" rx="0" ry="0" width="200" height="105" />
+      <rect x="268" y="50" rx="0" ry="0" width="270" height="18" />
+      <rect x="268" y="70" rx="0" ry="0" width="270" height="18" />
+      <rect x="268" y="30" rx="0" ry="0" width="270" height="18" />
+      {/* <rect x="362" y="33" rx="0" ry="0" width="51" height="16" /> */}
+      <rect x="560" y="25" rx="0" ry="0" width="137" height="20" />
+      <rect x="560" y="50" rx="0" ry="0" width="137" height="20" />
+      <rect x="560" y="75" rx="0" ry="0" width="137" height="20" />
+      <rect x="740" y="45" rx="0" ry="0" width="172" height="30" />
+      <rect x="950" y="40" rx="0" ry="0" width="150" height="50" />
+    </ContentLoader>
+  );
+}
+
+export { Skeleton };
--- src/pages/RentCarPage.jsx
+++ src/pages/RentCarPage.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchCars } from '../store/carsSlice';
 import { CarCard } from '../components/CarCard';
+import { Skeleton } from '../components/CarCard/Skeleton';
 
 function RentCarPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useDispatch();
 
   const cars = useSelector((state) => state.cars.cars);
@@ -15,15 +18,16 @@ function RentCarPage() {
     } catch (error) {
       console.error(error);
     }
+    setIsLoading(false);
   }, [dispatch]);
 
   return (
     <div className="rent-car__wrapper">
       <h2>Выберите автомобиль для аренды:</h2>
       <div className="rent-car__items">
-        {cars.map((car) => (
-          <CarCard key={car.id} {...car} />
-        ))}
+        {isLoading
+          ? [...Array(3)].map((_, i) => <Skeleton key={i} />)
+          : cars.map((car) => <CarCard key={car.id} {...car} />)}
       </div>
     </div>
   );
